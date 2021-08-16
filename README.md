baee77487506ff1430e22a61eab56c8d76006d94
Ayush <85835266+ayush-alam@users.noreply.github.com>
2021-08-16 13:13:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+<!-- This README.md file is developed by Ayush, Apple On Head. -->
+
+<!-- The Heading and the real content -->
+# How to write a README file
+In this page, I will try to explain you how to write a great README file.
+Hoping that after this tutorial, you can make a awesome README for you repository!
+***
+**Contents**
+#### [Headings](https://ayush-alam.github.io/readme/#1)
+#### [Normal text](https://ayush-alam.github.io/readme/#2)
+#### [Italics](https://ayush-alam.github.io/readme/#3)
+#### [Bold text](https://ayush-alam.github.io/readme/#4)
+#### [Strikethrough](https://ayush-alam.github.io/readme/#5)
+#### [Hyperlinks](https://ayush-alam.github.io/readme/#6)
+#### [Images](https://ayush-alam.github.io/readme/#7)
+#### [Table](https://ayush-alam.github.io/readme/#8)
+#### [List](https://ayush-alam.github.io/readme/#9)
+#### [Quote](https://ayush-alam.github.io/readme/#10)
+#### [Code](https://ayush-alam.github.io/readme/#11)
+#### [Code Block](https://ayush-alam.github.io/readme/#12)
+#### [Horizontal Line](https://ayush-alam.github.io/readme/#13)
+***
+**_-Ayush_** 
---
+++ script.js
@@ -0,0 +1,129 @@
+function b1(){
+    document.getElementById('b1').style.backgroundColor='white';
+    document.getElementById('b1').style.color='black';
+}
+
+function b12(){
+    document.getElementById('b1').style.backgroundColor='black';
+    document.getElementById('b1').style.color='white';
+}
+
+function b2(){
+    document.getElementById('b2').style.backgroundColor='white';
+    document.getElementById('b2').style.color='black';
+}
+
+function b22(){
+    document.getElementById('b2').style.backgroundColor='black';
+    document.getElementById('b2').style.color='white';
+}
+
+function b3(){
+    document.getElementById('b3').style.backgroundColor='white';
+    document.getElementById('b3').style.color='black';
+}
+
+function b32(){
+    document.getElementById('b3').style.backgroundColor='black';
+    document.getElementById('b3').style.color='white';
+}
+
+function b4(){
+    document.getElementById('b4').style.backgroundColor='white';
+    document.getElementById('b4').style.color='black';
+}
+
+function b42(){
+    document.getElementById('b4').style.backgroundColor='black';
+    document.getElementById('b4').style.color='white';
+}
+
+function b5(){
+    document.getElementById('b5').style.backgroundColor='white';
+    document.getElementById('b5').style.color='black';
+}
+
+function b52(){
+    document.getElementById('b5').style.backgroundColor='black';
+    document.getElementById('b5').style.color='white';
+}
+
+function b6(){
+    document.getElementById('b6').style.backgroundColor='white';
+    document.getElementById('b6').style.color='black';
+}
+
+function b62(){
+    document.getElementById('b6').style.backgroundColor='black';
+    document.getElementById('b6').style.color='white';
+}
+
+function b7(){
+    document.getElementById('b7').style.backgroundColor='white';
+    document.getElementById('b7').style.color='black';
+}
+
+function b72(){
+    document.getElementById('b7').style.backgroundColor='black';
+    document.getElementById('b7').style.color='white';
+}
+
+function b8(){
+    document.getElementById('b8').style.backgroundColor='white';
+    document.getElementById('b8').style.color='black';
+}
+
+function b82(){
+    document.getElementById('b8').style.backgroundColor='black';
+    document.getElementById('b8').style.color='white';
+}
+
+function b9(){
+    document.getElementById('b9').style.backgroundColor='white';
+    document.getElementById('b9').style.color='black';
+}
+
+function b92(){
+    document.getElementById('b9').style.backgroundColor='black';
+    document.getElementById('b9').style.color='white';
+}
+
+function b10(){
+    document.getElementById('b10').style.backgroundColor='white';
+    document.getElementById('b10').style.color='black';
+}
+
+function b102(){
+    document.getElementById('b10').style.backgroundColor='black';
+    document.getElementById('b10').style.color='white';
+}
+
+function b11(){
+    document.getElementById('b11').style.backgroundColor='white';
+    document.getElementById('b11').style.color='black';
+}
+
+function b112(){
+    document.getElementById('b11').style.backgroundColor='black';
+    document.getElementById('b11').style.color='white';
+}
+
+function b121(){
+    document.getElementById('b12').style.backgroundColor='white';
+    document.getElementById('b12').style.color='black';
+}
+
+function b122(){
+    document.getElementById('b12').style.backgroundColor='black';
+    document.getElementById('b12').style.color='white';
+}
+
+function b131(){
+    document.getElementById('b13').style.backgroundColor='white';
+    document.getElementById('b13').style.color='black';
+}
+
+function b132(){
+    document.getElementById('b13').style.backgroundColor='black';
+    document.getElementById('b13').style.color='white';
+}
